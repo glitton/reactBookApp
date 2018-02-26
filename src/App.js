467751351110a8bc1572a0cad9f2fa8442540934
Generosa Litton <glcodeworks@gmail.com>
2018-02-25 21:53:30 -0800

    first commit static version wip

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+// import * as BooksAPI from './BooksAPI'
+import './App.css';
+import BooksInShelf from './BooksInShelf';
+
+const books = [
+  {id: 1, title: 'To Kill a Mockingbird', authors: 'Harper Lee', shelf: 'currentlyReading', imageLinks: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'},
+  {id: 2, title: 'Enders Game', authors: 'Orson Scott Card', shelf: 'currentlyReading', imageLinks: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'}
+]
+
+const shelfTitle = ['Currently Reading', 'Want To Read', 'Read'];
+
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        ) : (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{shelfTitle[0]}</h2>
+                  <div className="bookshelf-books">
+                    <BooksInShelf books={books}/>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{shelfTitle[1]}</h2>
+                  <BooksInShelf books={books} />
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">{shelfTitle[2]}</h2>
+                  <div className="bookshelf-books">
+                    <BooksInShelf books={books}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BooksApp
